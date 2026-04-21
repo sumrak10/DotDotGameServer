94170e20b901bbc13ff08b458c7ae5bc6062316b
Nurlan Urazkulov <nurlan.urazkulov@dsh.pro>
2026-04-21 17:19:17 +0500

    version 0.4.1

--- gen.go
+++ gen.go
@@ -1,7 +1,2 @@
 //go:generate protoc -I=proto --go_out=pkg/pb/go --go_opt=paths=source_relative proto/game/input.proto proto/game/output.proto proto/game/world/world.proto proto/game/world/nodes/nodes.proto
-
-//js:generate protoc -I=proto --js_out=import_style=commonjs,binary:pkg/pb/js --grpc-web_out=import_style=typescript,mode=grpcwebtext:pkg/pb/js proto/game/input.proto proto/game/output.proto proto/game/world/world.proto proto/game/world/nodes/nodes.proto
-//js:generate protoc -I=proto --js_out=import_style=closure,binary:pkg/pb/js proto/game/input.proto proto/game/output.proto proto/game/world/world.proto proto/game/world/nodes/nodes.proto
-//js:generate protoc -I=proto --js_out=library=pkg/pb/js/game,binary:. proto/game/input.proto proto/game/output.proto proto/game/world/world.proto proto/game/world/nodes/nodes.proto
-
 package main
